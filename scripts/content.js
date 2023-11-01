eab39a4420276c0d070fa45b733de0920f9c0622
RobinYonathan <robin@robinyonathan.com>
2023-11-01 11:12:26 +0700

    test

--- scripts/content.js
+++ scripts/content.js
@@ -12,36 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const article = document.querySelector('article');
+console.log("This is a popup!");
+document.getElementById("myButton").addEventListener("click", myFunction);
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  /**
-   * Regular expression to find all "words" in a string.
-   *
-   * Here, a "word" is a sequence of one or more non-whitespace characters in a row. We don't use the
-   * regular expression character class "\w" to match against "word characters" because it only
-   * matches against the Latin alphabet. Instead, we match against any sequence of characters that
-   * *are not* a whitespace characters. See the below link for more information.
-   *
-   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-   */
-  const wordMatchRegExp = /[^\s]+/g;
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement('p');
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add('color-secondary-text', 'type--caption');
-  badge.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = article.querySelector('h1');
-  // Support for article docs with date
-  const date = article.querySelector('time')?.parentNode;
-
-  // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
-  (date ?? heading).insertAdjacentElement('afterend', badge);
+async function myFunction() {
+//     const permissionsToRequest = {
+//     permissions: ["USB"],
+//     origins: ["https://connect.dev.ittron.co.id/"],
+//   };
+//     const response = await chrome.permissions.request(permissionsToRequest);
+//     if (response) {
+//         document.getElementById("myButton").innerHTML = 'response';
+//     } else {
+//         document.getElementById("myButton").innerHTML = 'Permission was refused';
+//       console.log("Permission was refused");
+//     }
+    document.getElementById("myButton").innerHTML = 'xxxxx';
+    try {
+        navigator.usb.getDevices().then(devices => {
+            document.getElementById("myButton").innerHTML = 'devices' + devices.length;
+            devices.forEach(device => {
+                document.getElementById("myButton").innerHTML = 'device.productName' + device.productName;
+              console.log(device.productName);      // "Arduino Micro"
+              console.log(device.manufacturerName); // "Arduino LLC"
+            });
+          });
+          chrome.printing.getPrinters().then((printers) => {});
+    } catch (error) {
+        document.getElementById("myButton").innerHTML = 'errror';
+    } finally {
+    }
 }
